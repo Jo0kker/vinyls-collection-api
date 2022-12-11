6a701393a73f4a9abfe56c465dee769486b3da70
Benjamin <boublou@hotmail.fr>
2022-12-11 21:27:46 +0100

    :sparkles: discogs feature

--- app/Http/Controllers/SearchesController.php
+++ app/Http/Controllers/SearchesController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\VinylCollection;
 use App\Models\Search;
 use Orion\Http\Controllers\Controller as Controller;
 
 class SearchesController extends Controller
 {
     protected $model = Search::class;
+
+    protected $collectionResource = VinylCollection::class;
 
     public function includes(): array
     {
---
+++ app/Services/DiscogsService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+class DiscogsService
+{
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client([
+            'base_uri' => 'https://api.discogs.com/',
+            'headers' => [
+                'User-Agent' => 'VinylApp/0.1 +https://vinylapp.com',
+            ]
+        ]);
+
+    }
+
+    public function getVinylData($artist, $title)
+    {
+        $response = $this->client->request('GET', 'database/search', [
+            'query' => [
+                'q' => $artist . ' ' . $title,
+                'type' => 'release',
+                'per_page' => 1,
+                'token' => env('DISCOGS_TOKEN'),
+            ]
+        ]);
+        return json_decode($response->getBody()->getContents(), false, 512, JSON_THROW_ON_ERROR);
+    }
+
+    public function getVinylDataById($id)
+    {
+        $response = $this->client->request('GET', 'releases/' . $id, [
+            'query' => [
+                'token' => env('DISCOGS_TOKEN'),
+            ]
+        ]);
+        return json_decode($response->getBody()->getContents(), false, 512, JSON_THROW_ON_ERROR);
+    }
+}
