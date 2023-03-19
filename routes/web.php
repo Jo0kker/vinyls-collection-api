66edcb0809b071c8051d40a7c7c34a658b743f6c
Jo0kker <boublou@hotmail.fr>
2023-03-19 22:27:12 +0100

    change passport config

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('/test', function () {
-//    $response = Http::asForm()->post('http://localhost:8080/oauth/token', [
-//        "grant_type" => "password",
-//        "client_id" => "2",
-//        "client_secret" => "F0XckSdexv1TDvYLZeC4BWWTou351rmaM0ViDO6G",
-//        "username" => "test@example.com",
-//        "password" => "password",
-//        "scope" => "",
-//    ]);
-//
-//    return $response->json();
-//});
