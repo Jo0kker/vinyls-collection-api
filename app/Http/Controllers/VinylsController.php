4d4ce1800d636349340ded9caa682a80f0732588
Jo0kker <boublou@hotmail.fr>
2023-11-27 06:02:13 +0100

    fix get image user agent

--- app/Http/Controllers/VinylsController.php
+++ app/Http/Controllers/VinylsController.php
@@ -40,7 +40,15 @@ class VinylsController extends Controller
         // save image to storage
         $image = $vinyl['image'];
         $image = str_replace('http://', 'https://', $image);
-        $image = file_get_contents($image);
+        // context change user agent
+        $opts = [
+            'http' => [
+                'method' => 'GET',
+                'header' => 'User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n',
+            ],
+        ];
+        $context = stream_context_create($opts);
+        $image = file_get_contents($image, false, $context);
         // get folder in config env
 
         $storageSystem = config('filesystems.default');
