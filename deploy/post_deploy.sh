a13eac72321a20a010c4baad2665a899bda65d1c
Jo0kker <boublou@hotmail.fr>
2024-11-29 12:40:43 +0100

    update deploy script

--- deploy/post_deploy.sh
+++ deploy/post_deploy.sh
@@ -1,38 +1,6 @@
 #!/bin/bash -l
 
-echo "Running post-build tasks on the main instance (Instance 0)..."
-
-# Configurer s3cmd pour Cellar Clever Cloud
-S3_BUCKET="s3://vinyl-collection" # Remplace par ton bucket Cellar
-S3CMD_CONFIG_FILE="s3cmd.conf"
-
-cat > $S3CMD_CONFIG_FILE <<EOL
-[default]
-access_key = ${AWS_ACCESS_KEY_ID}
-secret_key = ${AWS_SECRET_ACCESS_KEY}
-host_base = ${AWS_ENDPOINT}
-host_bucket = ${AWS_ENDPOINT}
-signature_v2 = False
-use_https = True
-EOL
-
-# Vérifier si les clés Passport existent localement
-if [ ! -f storage/oauth-private.key ] || [ ! -f storage/oauth-public.key ]; then
-    echo "Passport keys not found locally. Fetching from S3..."
-    
-    # Récupérer les clés depuis Clever Cloud S3
-    s3cmd -c $S3CMD_CONFIG_FILE get $S3_BUCKET/oauth-private.key storage/oauth-private.key
-    s3cmd -c $S3CMD_CONFIG_FILE get $S3_BUCKET/oauth-public.key storage/oauth-public.key
-
-    # Vérifier si les clés ont bien été récupérées
-    if [ -f storage/oauth-private.key ] && [ -f storage/oauth-public.key ]; then
-        echo "Passport keys successfully fetched from S3."
-        chmod 640 storage/oauth-private.key
-        chmod 644 storage/oauth-public.key
-    fi
-else
-    echo "Passport keys already exist locally. Skipping generation."
-fi
+echo "Running post-build tasks"
 
 # Exécuter les migrations
 echo "Running migrations..."
